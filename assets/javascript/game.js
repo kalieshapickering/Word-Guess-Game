7e3d63c992b922c3d71ca4a6a4f4278e5ce61172
kalistan <kaliesha.pickering@gmail.com>
2018-07-11 17:46:40 -0400

    updated script to log a random artist to the console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,38 @@
 //pop-up window letting user know to press any key to start
-var startAlert = alert("Press any key to start");
+var startGame = alert("Press any key to start");
 
-// wordbank
+// wordbank, word options
 
 var wordBank = ["rich the kid", "travis scott", "xxxtentacion", "young thug", "lil pump"];
-//pictures
-
-//pick a random artist
-var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-//audio
-
-this.richTheKidAudio = new Audio(),
-this.travisScotAudio = new Audio(),
-this.xxxtentacionAudio = new Audio(),
-this.youngThugAudio = new Audio(),
-this.lilPumpAudio = new Audio(),
 
 
+//pick a random artist 
+var getWord = function(wordBank){
+return wordBank[Math.floor(Math.random() * wordBank.length)];
+}
+var randomArtist = getWord(wordBank);
+console.log(randomArtist)
 
-// word to guess
+
+// correct letter guesses
+
+var correctCharacter = [];
 
 
 // # of guesses remaining
 
 
 //letters already guessed
-
+var wrongCharacter = [];
 
 // win function
 
-function(){
 
-        this.playSound("richTheKidAudio");
-}
+//pictures
+
+
+//audio
+
 
 //lose function
 
- function(){
-    this.msgTitle.html("You have failed the Trap Gods");
-    this.msgText.html("Try your luck again!")
-}
