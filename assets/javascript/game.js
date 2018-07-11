9f2177b506b50852b1c03dba3c4e764369724649
kalistan <kaliesha.pickering@gmail.com>
2018-07-11 15:02:06 -0400

    alert and wordbank array added to code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,22 @@
 //pop-up window letting user know to press any key to start
-//var startAlert = alert("Press any key to start");
+var startAlert = alert("Press any key to start");
 
+// wordbank
+
+var wordBank = ["rich the kid", "travis scott", "xxxtentacion", "young thug", "lil pump"];
 //pictures
 
+//pick a random artist
+var word = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-//title of artist
+//audio
+
+this.richTheKidAudio = new Audio(),
+this.travisScotAudio = new Audio(),
+this.xxxtentacionAudio = new Audio(),
+this.youngThugAudio = new Audio(),
+this.lilPumpAudio = new Audio(),
+
 
 
 // word to guess
@@ -14,3 +26,18 @@
 
 
 //letters already guessed
+
+
+// win function
+
+function(){
+
+        this.playSound("richTheKidAudio");
+}
+
+//lose function
+
+ function(){
+    this.msgTitle.html("You have failed the Trap Gods");
+    this.msgText.html("Try your luck again!")
+}
