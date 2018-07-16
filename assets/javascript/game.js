b48a1ed202bb9a28c2707c3996e4cb4f862242f1
kalistan <kaliesha.pickering@gmail.com>
2018-07-16 13:05:24 -0400

    update javascript functions and logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,23 @@
 //game score starts at 0
-var score = 0;
+var wins = 0;
+var loses = 0;
+var gamesPlayed = 0;
 
 // wordbank, word options
 
-var wordBank = ["rich the kid", "travis scott", "xxxtentacion", "young thug", "lil pump"];
+var wordBank = [
+"rich the kid",
+"travis scott", 
+"xxxtentacion",
+"young thug", 
+"lil pump"
+];
 
-//functions of the word guess game render a new question, update score, play music on correct guess, update letters already guess, update reaming guesses
+//functions of the word guess game render a new question, update score, play music on correct guess, update letters already guess, update reaming guesses, updates score
 
 // render a new question
  function renderQuestion(){
-     if (workBankIndex <= (wordBank.length -1)){
+     if (randonArtist <= (wordBank.length -1)){
          document.querySelector("#randomArtist").innerHTML = randomArtist;
         // if there is no more options 
      }else{
@@ -21,7 +29,14 @@ var wordBank = ["rich the kid", "travis scott", "xxxtentacion", "young thug", "l
 var getWord = function(wordBank){
     return wordBank[Math.floor(Math.random() * wordBank.length)];
     }
-    var randomArtist = getWord(wordBank);
+    var randomArtist = getWord();
+    var displayArtist = [];
+    var lettersSelectedByUser =[];
+
+//update letter choice with "_"
+    for(i=0; i < randomArtist.length; i++){
+        displayArtist.push(randomArtist[i].replace(randomArtist[i],"_"));
+    }
  // update score
     function updateScore() {
         document.querySelector("#wins").innerHTML = wins;
@@ -32,10 +47,10 @@ var getWord = function(wordBank){
         document.querySelector("#image").innerHTML = ("imageLocation?");
     }
 
-//correct character update
+//correct characters
 var correctCharacter = [];
 
-// change letters guessed
+// letters guessed
 var wrongCharacter = [];
 
 //change guesses remaining
@@ -43,6 +58,7 @@ var maxCount = 12;
 var count = 0;
 var guessesLeft = maxCount - count;
 document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+
 
 //remove selected word from rotation
 
@@ -55,12 +71,52 @@ updateScore();
 //playing the game
 document.onkeyup = function ( event){
     var userGuess = event.key.toLowerCase();
+    var guessLeft = maxCount - count;
+document.querySelector("guessesLeft").innerHTML = guessesLeft;
+function checkForWinner(){
+    if (displayArtist.randomArtist("_") !== -1){
+        if (count === maxCount){
+            //lose
+        }
+    }
+    if (displayArtist.randomArtist("_") === -1){
+        //win
+    }
+}
+   //correct character
+   if(userGuess){
+       if(count !== maxCount){
+        for(i=0; i < randomArtist.length; i++){
+            if (userGuess === randomArtist.charAt(i)){
+                count++
+                guessesLeft--
+                document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+                document.querySelector("#guessesOutcome").innerHTML = "Correct!";
+                displayArtist[i] = userGuess;
+                document.querySelector("#displayArtist").innerHTML = displayArtist.join("_");
+                checkForWinner();
+            }
+       }
+       //wrong character
+       if (randomArtist.indexOf(userGuess) === -1){
+           count++;
+           guessesLeft --;
+           lettersSelectedByUser.push(userGuess);
+           document.querySelector("#alreadyGuessed").innerHTML = wrongCharacter.join("_");
+           document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+           document.querySelector("#guessesOutcome").innerHTML = "Wrong!";
+          
+           checkForWinner();
+       }
+   }
 
-    if (userGuess )
+    
     // game display next question
-randomArtist++;
-renderQuestion();
+    RandomArtist++;
+    renderQuestion();
+    gamesPlayed++;
 
+}
 }
 
 
